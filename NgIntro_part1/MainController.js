47fde3a6c1914a6ad57cc1244be83a6981ec0bdf
Monomachus <monomachus85@yahoo.com>
2015-01-06 14:21:49 +0200

    Finalized code after watching course on Pluralsight

---
+++ NgIntro_part1/MainController.js
@@ -0,0 +1,35 @@
+﻿(function () {
+
+    var app = angular.module("githubViewer");
+
+    var MainController = function ($scope, $interval, $location) {
+
+        var decrementCountdown = function() {
+            $scope.countdown -= 1;
+            if ($scope.countdown < 1) {
+                $scope.search($scope.username);
+            }
+        }
+
+        var countdownIntervall = null;
+
+        var startCountdown = function () {
+            countdownIntervall = $interval(decrementCountdown, 1000, $scope.countdown);
+        }
+
+        $scope.search = function (username) {
+            if (countdownIntervall) {
+                $interval.cancel(countdownIntervall);
+                $scope.countdown = null;
+            }
+            $location.path("/user/" + username);
+        }
+
+        $scope.username = "angular";
+        $scope.countdown = 5;
+        
+        startCountdown();
+    }
+
+    app.controller("MainController", MainController);
+}());
---
+++ NgIntro_part1/RepoController.js
@@ -0,0 +1,29 @@
+﻿
+(function () {
+    var app = angular.module("githubViewer");
+
+    var RepoController = function ($scope, github, $routeParams) {
+
+        var onRepoComplete = function (data) {
+            $scope.repo = data;
+            github.getRepoContributors(username, reponame).then(onContributors, onError);
+        };
+
+        var onContributors = function (data) {
+            $scope.contributors = data;
+        };
+
+        var onError = function (reason) {
+            $scope.error = "Could not fetch the data";
+        };
+
+        var username = $routeParams.username;
+        var reponame = $routeParams.reponame;
+
+        github.getRepoDetails(username, reponame)
+              .then(onRepoComplete, onError);
+    };
+
+    app.controller("RepoController", RepoController);
+
+}())
